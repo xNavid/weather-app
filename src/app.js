464746e48c86bbd46ba092e1f7ff2d02e375b28b
Navid <navidcaesar@gmail.com>
2019-05-29 17:32:58 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const hbs  = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Home', 
+        name: 'Jack'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'Please provide a search term.'
+        })
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location} = {})=>{
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData)=> {
+            if (error) {
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'Please provide a search term.'
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found!'
+    })
+}) 
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
