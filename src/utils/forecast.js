79a8d9d701dc20d42888c46b4c373f40d03cd3c5
Navid <navidcaesar@gmail.com>
2019-06-10 10:50:26 +0800

    Make modifcations to header, about and forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forcast = (latitude, longitude, callback) => {
             callback('Unable to find location.', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high today is ' + body.daily.data[0].temperatureHigh + ', with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
